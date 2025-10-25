e638997a7406a7a0706a9a411f28ba27cd22ad20
eunseo <2023180012@tukorea.ac.kr>
2025-10-26 01:51:15 +0900

    shaderMaker 분할

--- CG_20251026/main.cpp
+++ CG_20251026/main.cpp
@@ -5,17 +5,15 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
 #include "filetobuf.h"
+#include "shaderMaker.h"
 
 void make_vertexShaders();
 void make_fragmentShaders();
 GLuint make_shaderProgram();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
-GLint width = 900, height = 900;
-GLuint shaderProgramID;
-GLuint vertexShader;
-GLuint fragmentShader;
 
 void main(int argc, char** argv)
 {
@@ -23,7 +21,7 @@ void main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(width, height);
-	glutCreateWindow("Test_glsl");
+	glutCreateWindow("Tesk_19");
 
 	glewExperimental = GL_TRUE;
 	glewInit();
@@ -34,68 +32,6 @@ void main(int argc, char** argv)
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutMainLoop();
-}
-
-void make_vertexShaders()
-{
-	GLchar* vertexSource;
-	vertexSource = filetobuf("vertex.glsl");
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
-	glCompileShader(vertexShader);
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-		std::cerr << "Error: vertex shader            \n" << errorLog << std::endl;
-		return;
-	}
-}
-
-void make_fragmentShaders()
-{
-	GLchar* fragmentSource;
-	fragmentSource = filetobuf("fragment.glsl");
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	glCompileShader(fragmentShader);
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: frag_shader            \n" << errorLog << std::endl;
-		return;
-	}
-}
-
-GLuint make_shaderProgram()
-{
-	GLint result;
-	GLchar* errorLog = NULL;
-	GLuint shaderID;
-	shaderID = glCreateProgram();
-
-	glAttachShader(shaderID, vertexShader);
-	glAttachShader(shaderID, fragmentShader);
-
-	glLinkProgram(shaderID);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	glGetProgramiv(shaderID, GL_LINK_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
-		std::cerr << "ERROR: shader program          \n" << errorLog << std::endl;
-		return false;
-	}
-	glUseProgram(shaderID);
-	return shaderID;
 }
 
 GLvoid drawScene()
