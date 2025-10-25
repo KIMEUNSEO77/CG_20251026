893d314ac9816126cb6fbded1e10aa56fe8ce882
eunseo <2023180012@tukorea.ac.kr>
2025-10-26 02:26:09 +0900

    변환 함수들 작성

--- CG_20251026/main.cpp
+++ CG_20251026/main.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
 #include <glew.h>
 #include <freeglut.h>
 #include <freeglut_ext.h> 
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include "filetobuf.h"
 #include "shaderMaker.h"
+#include "obj_load.h"
 
 void make_vertexShaders();
 void make_fragmentShaders();
@@ -15,37 +12,73 @@ GLuint make_shaderProgram();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 
-void main(int argc, char** argv)
+Mesh gSphere;
+
+int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);  // 깊이 버퍼 추가
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(width, height);
 	glutCreateWindow("Tesk_19");
 
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+
 	glewExperimental = GL_TRUE;
 	glewInit();
+
+	glEnable(GL_DEPTH_TEST); // 깊이 테스트 활성화
 
 	make_vertexShaders();
 	make_fragmentShaders();
 	shaderProgramID = make_shaderProgram();
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(Reshape);
+
+	// obj 파일 로드
+	if (!LoadOBJ_PosNorm_Interleaved("sphere.obj", gSphere)) 
+	{
+		std::cerr << "Failed to load sphere.obj\n";
+		return 1;
+	}
+
 	glutMainLoop();
+	return 0;
 }
 
 GLvoid drawScene()
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(shaderProgramID);
+	glBindVertexArray(gSphere.vao);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	GLint modelLoc = glGetUniformLocation(shaderProgramID, "model");
+	GLint viewLoc = glGetUniformLocation(shaderProgramID, "view");
+	GLint projLoc = glGetUniformLocation(shaderProgramID, "projection");
+
+	glm::mat4 mTransform = glm::mat4(1.0f);
+	mTransform = glm::rotate(mTransform, glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &mTransform[0][0]);
+
+	glm::mat4 vTransform = glm::mat4(1.0f);
+	vTransform = glm::translate(vTransform, glm::vec3(0.0f, 0.0f, -3.0f));
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &vTransform[0][0]);
+
+	glm::mat4 pTransform = glm::mat4(1.0f);
+	pTransform = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
+
+	glBindVertexArray(gSphere.vao);
+	glDrawArrays(GL_TRIANGLES, 0, gSphere.count);
+	glBindVertexArray(0);
+
 	glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h)
 {
+	width = w;
+	height = h;
 	glViewport(0, 0, w, h);
 }
--- CG_20251026/vertex.glsl
+++ CG_20251026/vertex.glsl
@@ -1,11 +1,16 @@
 #version 330 core
-layout(location=0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;   // ← VAO와 일치
 
-uniform mat4 uModel;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 out vec3 vColor;
 
-void main(){
-    gl_Position = uModel * vec4(aPos, 1.0);
-    vColor = aColor; 
+void main() {
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+
+    // 노멀로 임시 컬러 만들기(시각 확인용)
+    vColor = normalize(aNormal) * 0.5 + 0.5; // [-1,1] → [0,1]
 }
