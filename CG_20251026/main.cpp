e7807ef3b99484bfd1f766bf5094a18598381a11
eunseo <2023180012@tukorea.ac.kr>
2025-10-27 03:00:03 +0900

    구들을 화면에 그림

--- CG_20251026/main.cpp
+++ CG_20251026/main.cpp
@@ -12,7 +12,18 @@ GLuint make_shaderProgram();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 
-Mesh gSphere;
+Mesh gSphere;   // 중심의 구
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':
+		exit(0);
+		break;
+	}
+}
+
 
 int main(int argc, char** argv)
 {
@@ -29,6 +40,7 @@ int main(int argc, char** argv)
 	glewInit();
 
 	glEnable(GL_DEPTH_TEST); // 깊이 테스트 활성화
+	glEnable(GL_CULL_FACE);  // 은면 제거 활성화
 
 	make_vertexShaders();
 	make_fragmentShaders();
@@ -41,8 +53,20 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 	return 0;
+}
+
+// 구 그리는 함수
+void DrawSphere(const Mesh& mesh, GLuint shaderProgram, const glm::mat4& model)
+{
+	GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
+
+	glBindVertexArray(mesh.vao);
+	glDrawArrays(GL_TRIANGLES, 0, mesh.count);
+	glBindVertexArray(0);
 }
 
 GLvoid drawScene()
@@ -51,19 +75,13 @@ GLvoid drawScene()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(shaderProgramID);
-	glBindVertexArray(gSphere.vao);
 
-	GLint modelLoc = glGetUniformLocation(shaderProgramID, "model");
 	GLint viewLoc = glGetUniformLocation(shaderProgramID, "view");
 	GLint projLoc = glGetUniformLocation(shaderProgramID, "projection");
 
-	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, -5.0f);
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 8.0f);
 	glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
-	glm::mat4 mTransform = glm::mat4(1.0f);
-	mTransform = glm::rotate(mTransform, glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &mTransform[0][0]);
 
 	glm::mat4 vTransform = glm::mat4(1.0f);
 	vTransform = glm::lookAt(cameraPos, cameraDirection, cameraUp);
@@ -73,9 +91,22 @@ GLvoid drawScene()
 	pTransform = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
 
-	glBindVertexArray(gSphere.vao);
-	glDrawArrays(GL_TRIANGLES, 0, gSphere.count);
-	glBindVertexArray(0);
+	// 중심 구 (기존과 동일)
+	glm::mat4 center = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	center = glm::scale(center, glm::vec3(2.0f, 2.0f, 2.0f));
+	DrawSphere(gSphere, shaderProgramID, center);
+
+	// 오른쪽에 구
+	glm::mat4 m1 = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, 0.0f));
+	DrawSphere(gSphere, shaderProgramID, m1);
+
+	// 왼쪽 아래 구
+	glm::mat4 m2 = glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, -2.0f, 0.0f));
+	DrawSphere(gSphere, shaderProgramID, m2);
+
+	// 오른쪽 아래 구
+	glm::mat4 m3 = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, -2.0f, 0.0f));
+	DrawSphere(gSphere, shaderProgramID, m3);
 
 	glutSwapBuffers();
 }
