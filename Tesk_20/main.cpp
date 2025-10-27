9dd79632ea91de325124c20e31e87377f2d507ee
eunseo <2023180012@tukorea.ac.kr>
2025-10-28 00:57:44 +0900

    탱크 이동 (방향키 명령어)

--- Tesk_20/main.cpp
+++ Tesk_20/main.cpp
@@ -16,6 +16,28 @@ GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 
 Mesh gTank;
+float moveX = 0.0f;
+float moveZ = 0.0f;
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q': exit(0); break;
+	}
+}
+
+void SpecialKeyboard(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP: moveZ -= 0.1f; break;
+	case GLUT_KEY_DOWN: moveZ += 0.1f; break;
+	case GLUT_KEY_LEFT: moveX -= 0.1f; break;
+	case GLUT_KEY_RIGHT: moveX += 0.1f; break;
+	}
+	glutPostRedisplay();
+}
 
 int main(int argc, char** argv)
 {
@@ -43,6 +65,8 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(SpecialKeyboard);
 	glutMainLoop();
 	return 0;
 }
@@ -93,48 +117,56 @@ GLvoid drawScene()
 	glm::mat4 bottomBody = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 	bottomBody = glm::rotate(bottomBody, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	bottomBody = glm::rotate(bottomBody, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	bottomBody = glm::translate(bottomBody, glm::vec3(moveX, 0.0f, moveZ));
 	bottomBody = glm::scale(bottomBody, glm::vec3(3.0f, 0.5f, 1.0f));
 	DrawCube(gTank, shaderProgramID, bottomBody, glm::vec3(0.678f, 0.847f, 0.902f));
 	// Áß¾Ó ¸öÃ¼
 	glm::mat4 middleBody = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.3f, 0.0f));
 	middleBody = glm::rotate(middleBody, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	middleBody = glm::rotate(middleBody, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	middleBody = glm::translate(middleBody, glm::vec3(moveX, 0.0f, moveZ));
 	middleBody = glm::scale(middleBody, glm::vec3(1.5f, 0.25f, 0.5f));
 	DrawCube(gTank, shaderProgramID, middleBody, glm::vec3(0.564f, 0.933f, 0.564f));
 	// ¿ÞÂÊ À§ ¸öÃ¼
 	glm::mat4 topBody1 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.7f, 0.7f, 0.5f));
 	topBody1 = glm::rotate(topBody1, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	topBody1 = glm::rotate(topBody1, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	topBody1 = glm::translate(topBody1, glm::vec3(moveX, 0.0f, moveZ));
 	topBody1 = glm::scale(topBody1, glm::vec3(0.75f, 0.5f, 0.5f));
 	DrawCube(gTank, shaderProgramID, topBody1, glm::vec3(0.784f, 0.635f, 0.784f));
 	// ¿À¸¥ÂÊ À§ ¸öÃ¼
 	glm::mat4 topBody2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.7f, 0.7f, 0.5f));
 	topBody2 = glm::rotate(topBody2, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	topBody2 = glm::rotate(topBody2, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	topBody2 = glm::translate(topBody2, glm::vec3(moveX, 0.0f, moveZ));
 	topBody2 = glm::scale(topBody2, glm::vec3(0.75f, 0.5f, 0.5f));
 	DrawCube(gTank, shaderProgramID, topBody2, glm::vec3(0.784f, 0.635f, 0.784f));
 	// ¿ÞÂÊ ±ê´ë
 	glm::mat4 flag1 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.7f, 1.3f, 0.5f));
 	flag1 = glm::rotate(flag1, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	flag1 = glm::rotate(flag1, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	flag1 = glm::translate(flag1, glm::vec3(moveX, 0.0f, moveZ));
 	flag1 = glm::scale(flag1, glm::vec3(0.1f, 1.0f, 0.1f));
 	DrawCube(gTank, shaderProgramID, flag1, glm::vec3(1.0f, 0.7f, 0.3f));
 	// ¿À¸¥ÂÊ ±ê´ë
 	glm::mat4 flag2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.7f, 1.3f, 0.5f));
 	flag2 = glm::rotate(flag2, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	flag2 = glm::rotate(flag2, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	flag2 = glm::translate(flag2, glm::vec3(moveX, 0.0f, moveZ));
 	flag2 = glm::scale(flag2, glm::vec3(0.1f, 1.0f, 0.1f));
 	DrawCube(gTank, shaderProgramID, flag2, glm::vec3(1.0f, 0.7f, 0.3f));
 	// ¿ÞÂÊ Æ÷½Å
 	glm::mat4 barrel1 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.7f, 0.5f, 1.5f));
 	barrel1 = glm::rotate(barrel1, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	barrel1 = glm::rotate(barrel1, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	barrel1 = glm::translate(barrel1, glm::vec3(moveX, 0.0f, moveZ));
 	barrel1 = glm::scale(barrel1, glm::vec3(0.1f, 0.1f, 1.0f));
 	DrawCube(gTank, shaderProgramID, barrel1, glm::vec3(0.5f, 0.0f, 0.5f));
 	// ¿À¸¥ÂÊ Æ÷½Å
 	glm::mat4 barrel2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.7f, 0.5f, 1.5f));
 	barrel2 = glm::rotate(barrel2, glm::radians(-15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	barrel2 = glm::rotate(barrel2, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	barrel2 = glm::translate(barrel2, glm::vec3(moveX, 0.0f, moveZ));
 	barrel2 = glm::scale(barrel2, glm::vec3(0.1f, 0.1f, 1.0f));
 	DrawCube(gTank, shaderProgramID, barrel2, glm::vec3(0.5f, 0.0f, 0.5f));
 
@@ -143,5 +175,7 @@ GLvoid drawScene()
 
 GLvoid Reshape(int w, int h)
 {
+	width = w;
+	height = h;
 	glViewport(0, 0, w, h);
 }
